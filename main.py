361e3d9d965df6c78707b066c1ba8ae6d571bdf1
Matthew <mattjohnson@fanatics.com>
2017-04-03 10:09:16 -0600

    changed name to fan_bot

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ PIN_RIGHT_REV = 15
 motor_left = Motor(PIN_LEFT_FWD, PIN_LEFT_REV)
 motor_right = Motor(PIN_RIGHT_FWD, PIN_RIGHT_REV)
 
-fanatics_bot = Bot(motor_left, motor_right)
+fan_bot = Bot(motor_left, motor_right)
--- square.py
+++ square.py
@@ -7,19 +7,19 @@ TURN_TIME = 1
 TURN_SPEED = 3
 
 
-def run(fanatics_bot):
-	fanatics_bot.forward(LINE_SPEED)
+def run(fan_bot):
+	fan_bot.forward(LINE_SPEED)
 	sleep(LINE_TIME)
-	fanatics_bot.turn_left(TURN_SPEED)
+	fan_bot.turn_left(TURN_SPEED)
 	sleep(TURN_TIME)
-	fanatics_bot.forward(LINE_SPEED)
+	fan_bot.forward(LINE_SPEED)
 	sleep(LINE_TIME)
-	fanatics_bot.turn_left(TURN_SPEED)
+	fan_bot.turn_left(TURN_SPEED)
 	sleep(TURN_TIME)
-	fanatics_bot.forward(LINE_SPEED)
+	fan_bot.forward(LINE_SPEED)
 	sleep(LINE_TIME)
-	fanatics_bot.turn_left(TURN_SPEED)
+	fan_bot.turn_left(TURN_SPEED)
 	sleep(TURN_TIME)
-	fanatics_bot.forward(LINE_SPEED)
+	fan_bot.forward(LINE_SPEED)
 	sleep(LINE_TIME)
-	fanatics_bot.stop()
+	fan_bot.stop()
